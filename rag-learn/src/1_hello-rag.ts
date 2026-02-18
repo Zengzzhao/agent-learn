63213ba30ec728c6799eddc45ebff69544ba0ef2
zengzizhao <787332942@qq.com>
2026-02-18 23:45:30 +0800

    rag

---
+++ rag-learn/src/1_hello-rag.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { Document } from "@langchain/core/documents";
+import { MemoryVectorStore } from "@langchain/classic/verctorstores/memory";
+
+const model = new ChatOpenAI({
+  model: process.env.MODEL_NAME,
+  apiKey: process.env.OPENAPI_KEY,
+  configuration: {
+    baseURL: process.env.OPENAPI_BASE_URL,
+  },
+  temperature: 0,
+});
+
+// 嵌入模型
+// const embeddings=new OpenAIEmbeddings({
+//     apiKey:
+// })
+
